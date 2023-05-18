439be1042252aa4f355dcebffef14dbcc44eed35
HaruyaAnaguchi <131515772+HaruyaAnaguchi@users.noreply.github.com>
2023-05-18 13:39:19 +0000

    [Fix]Fuzzbuzz

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,21 +1,20 @@
-puts "数字を入力してください"
-input_num = gets.to_i
-
-a = input_num % 3
-b = input_num % 5
-
-def fuzz_buzz
-  if a == 0 && b != 0
-     "Fuzz"
-  elsif a != 0 && b == 0
-     "Buzz"
-  elsif a == 0 && b == 0
-     "FuzzBuzz"
-  else
-    "#{input_num}です"
-  end
+def fuzz_buzz(number)
+ if number % 3 == 0 && number % 5 != 0
+  "Fuzz"
+elsif number % 3 != 0 && number % 5 == 0
+  "Buzz"
+elsif number % 3 == 0 && number % 5 == 0
+  "FuzzBuzz"
+else
+  "#{number}"
+end
 end
 
-puts "#{fuzz_buzz}"
+puts "数字を入力してください"
+
+input = gets.to_i
+
+puts "結果は・・・"
+puts fuzz_buzz(input)
 
 
